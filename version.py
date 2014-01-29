abb95b20bce3a5bbf5ae46da0551023a121d3f2b
Aaron Hoover <amhoov@gmail.com>
2014-01-29 14:58:05 -0500

    Updated paths in version.py

--- version.py
+++ version.py
@@ -15,7 +15,7 @@ if len(sys.argv) < 2:
     print("Not enough arguments given. Need version string within single quotes.")
     sys.exit()
 
-filename   = 'source/version-string.h'
+filename   = 'version-string.h'
 fileheader = 'version.h'
 version    = sys.argv[1] # version string within single quotes
 date       = time.strftime("%a %b %d %H:%M:%S %Y")
